2e76a4ce1f1e06f337ebc029ca2b14c7d0d7d71d
hugasj <hjasinsk@stud.hs-heilbronn.de>
2023-12-13 16:11:52 +0200

    added random forest regression

--- model/ml_scripts.py
+++ model/ml_scripts.py
@@ -1,10 +1,12 @@
 import logging
 import numpy as np
 import sklearn.linear_model
+from sklearn.ensemble import RandomForestRegressor
 import sklearn
 import pandas as pd
 import os
 from init_scripts import init_logger
+from tqdm.notebook import tqdm
 
 
 def run_model():
@@ -25,11 +27,14 @@ def run_model():
     abp_sys_test = read_data('/features/testing/tot_abp_sys_test.csv')
     abp_dia_test = read_data('/features/testing/tot_abp_dia_test.csv')
 
-    run_linear_regression('SYS', ppg_sys_train, abp_sys_train, ppg_sys_test, abp_sys_test)
-    run_linear_regression('DIA', ppg_dia_train, abp_dia_train, ppg_dia_test, abp_dia_test)
+    # run_linear_regression('SYS', ppg_sys_train, abp_sys_train, ppg_sys_test, abp_sys_test)
+    # run_linear_regression('DIA', ppg_dia_train, abp_dia_train, ppg_dia_test, abp_dia_test)
 
-    run_sv_regression('SYS', ppg_sys_train, abp_sys_train, ppg_sys_test, abp_sys_test)
-    run_sv_regression('DIA', ppg_dia_train, abp_dia_train, ppg_dia_test, abp_dia_test)
+    # run_sv_regression('SYS', ppg_sys_train, abp_sys_train, ppg_sys_test, abp_sys_test)
+    # run_sv_regression('DIA', ppg_dia_train, abp_dia_train, ppg_dia_test, abp_dia_test)
+
+    run_random_forest('SYS', ppg_sys_train, abp_sys_train, ppg_sys_test, abp_sys_test)
+    run_random_forest('DIA', ppg_dia_train, abp_dia_train, ppg_dia_test, abp_dia_test)
 
 
 def read_data(path):
@@ -41,40 +46,32 @@ def read_data(path):
 
 
 def run_linear_regression(feat, ppg_train, abp_train, ppg_test, abp_test):
-    # Create and Fit model
-    lr_model = sklearn.linear_model.LinearRegression()
-    lr_model.fit(ppg_train.reshape(-1, 1), abp_train)
-
-    # Make predictions on the test set
-    predictions = lr_model.predict(ppg_test.reshape(-1, 1))
-
-    # Evaluate the model
-    mse = sklearn.metrics.mean_squared_error(abp_test, predictions)
-    logging.info(f'LR - Mean Squared Error: {mse} ({feat})')
-    mse = sklearn.metrics.mean_absolute_error(abp_test, predictions)
-    logging.info(f'LR - Mean Absolute Error: {mse} ({feat})')
-    bias = np.mean(predictions - abp_test)
-    logging.info(f'LR - Bias: {bias} ({feat})')
-    loa_l, loa_u = calculate_limits_of_agreement(predictions, abp_test)
-    logging.info(f'LR - Limits of Agreement: {loa_l, loa_u} ({feat})')
+    lr_model = sklearn.linear_model.LinearRegression(n_estimators=100)
+    mse, mae, r2, bias, loa = fit_predict_evaluate(lr_model, ppg_train, abp_train, ppg_test, abp_test)
+    logging.info(f'LR - MSE: {mse}, MAE: {mae}, R^2: {r2}, Bias: {bias}, LoA: {loa} ({feat})')
 
 
 def run_sv_regression(feat, ppg_train, abp_train, ppg_test, abp_test):
-    svr_model = sklearn.svm.SVR(kernel='rbf')
-    svr_model.fit(ppg_train.reshape(-1, 1), abp_train)
+    svr_model = sklearn.svm.SVR(kernel='sigmoid')
+    mse, mae, r2, bias, loa = fit_predict_evaluate(svr_model, ppg_train, abp_train, ppg_test, abp_test)
+    logging.info(f'SVR (sigmoid) - MSE: {mse}, MAE: {mae}, R^2: {r2}, Bias: {bias}, LoA: {loa} ({feat})')
 
-    # Make predictions on the test set
-    predictions = svr_model.predict(ppg_test.reshape(-1, 1))
 
-    # Evaluate the model
+def run_random_forest(feat, ppg_train, abp_train, ppg_test, abp_test):
+    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+    mse, mae, r2, bias, loa = fit_predict_evaluate(rf_model, ppg_train, abp_train, ppg_test, abp_test)
+    logging.info(f'RF - MSE: {mse}, MAE: {mae}, R^2: {r2}, Bias: {bias}, LoA: {loa} ({feat})')
+
+
+def fit_predict_evaluate(model, ppg_train, abp_train, ppg_test, abp_test):
+    model.fit(ppg_train.reshape(-1, 1), abp_train)
+    predictions = model.predict(ppg_test.reshape(-1, 1))
     mse = sklearn.metrics.mean_squared_error(abp_test, predictions)
-    logging.info(f'SVR - Mean Squared Error: {mse} ({feat})')
     mae = sklearn.metrics.mean_absolute_error(abp_test, predictions)
-    logging.info(f'SVR - Mean Absolute Error: {mae} ({feat})')
+    r2 = sklearn.metrics.r2_score(abp_test, predictions)
     bias = np.mean(predictions - abp_test)
-    logging.info(f'SVR - Bias: {bias} ({feat})')
     loa_l, loa_u = calculate_limits_of_agreement(predictions, abp_test)
-    logging.info(f'SVR - Limits of Agreement: {loa_l, loa_u} ({feat})')
+    return mse, mae, r2, bias, [loa_u, loa_u]
 
 
 def calculate_limits_of_agreement(pred, est):
